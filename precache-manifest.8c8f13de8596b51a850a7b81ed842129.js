d6a872001b37147d29ce303211f01d19a6ec5117
Peter Smiley <pdsmiley2@gmail.com>
2021-08-01 18:14:57 -0400

    Updates

--- precache-manifest.8c8f13de8596b51a850a7b81ed842129.js
+++ precache-manifest.8c8f13de8596b51a850a7b81ed842129.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2402c4b8f0c6d8d2e6f820fa24b80304",
+    "revision": "e7101b911987737fcedae3838eb8ca2d",
     "url": "/index.html"
   },
   {
-    "revision": "cde865a668d9ac566bee",
-    "url": "/static/js/2.262644b8.chunk.js"
+    "revision": "a2668462dd06077bd103",
+    "url": "/static/js/2.354c4937.chunk.js"
   },
   {
     "revision": "8a9a2ae4a25f202f52818f8f8074424e",
-    "url": "/static/js/2.262644b8.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.354c4937.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9a694a2c362346909072",
-    "url": "/static/js/main.81908cbd.chunk.js"
+    "revision": "0922b28f41eec82f894b",
+    "url": "/static/js/main.6b765274.chunk.js"
   },
   {
     "revision": "6981919806c089cef906",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2d7ff0b0dc9d5551c10715c50708258a.js"
+  "/precache-manifest.8c8f13de8596b51a850a7b81ed842129.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
